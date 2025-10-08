dd29ef714b820a116e53c279c7c87f232d6e1b05
Amin-ur-Rahman <aminurar679799@gmail.com>
2025-10-08 22:23:53 +0600

    app details page added

--- src/components/routes/appDetails/AppDetails.jsx
+++ src/components/routes/appDetails/AppDetails.jsx
@@ -13,7 +13,7 @@ function AppDetails() {
 
   let selectedApp = appData.find((app) => app.id == appId);
   if (!selectedApp) return;
-  const { title, image, description, downloads, ratingAvg, reviews } =
+  const { title, image, description, downloads, ratingAvg, reviews, size } =
     selectedApp;
 
   console.log(selectedApp);
@@ -21,29 +21,45 @@ function AppDetails() {
   return (
     <div id="main-layer" className="bg-[#f5f5f5]">
       <div id="second-layer" className="w-[90dvw] mx-auto py-20">
-        <div id="app-details" className="flex flex-col md:flex-row md:gap-10">
-          <div id="app-img">
+        <div
+          id="app-details"
+          className="flex flex-col items-center justify-center  md:flex-row md:gap-10 pb-5 border-b-2 border-gray-200 "
+        >
+          <div
+            id="app-img"
+            className="bg-white shadow-sm shadow-gray-500 w-max "
+          >
             <img
               src={image}
               alt="app image"
-              className="max-w-sm max-h-sm rounded-sm"
+              className="max-w-[300px] max-h-[300px]  object-cover"
             />
           </div>
-          <div id="app-info">
-            <h2>
+          <div id="app-info" className=" py-3 px-5  w-full">
+            <h2 className="text-lg font-bold">
               {title}: {description}
             </h2>
-            <p className="pb-6 border-b-2 border-gray-200">
-              Developed by <span className="text-violet-500">companyName</span>
+            <p className="pb-6 border-b-2 border-gray-200 text-gray-400">
+              Developed by{" "}
+              <span className="text-violet-500 font-semibold">companyName</span>
             </p>
-            <div className="pt-6">
-              <div>
+            <div className="pt-6 flex justify-between w-max md:gap-20">
+              <div className="flex flex-col gap-1 justify-center items-center">
                 <img src={downloadLogo} alt="downloadLogo" /> <p>downloads</p>{" "}
-                <p>{downloads}</p>
+                <p className="font-bold text-2xl">{downloads}</p>
               </div>
-              <div></div>
-              <div></div>
+              <div className="flex flex-col gap-1 justify-center items-center">
+                <img src={ratingLogo} alt="ratingLogo" /> <p>ratings</p>{" "}
+                <p className="font-bold text-2xl">{ratingAvg}</p>
+              </div>
+              <div className="flex flex-col gap-1 justify-center items-center">
+                <img src={reviewLogo} alt="reviewLogo" /> <p>reviews</p>{" "}
+                <p className="font-bold text-2xl">{reviews}</p>
+              </div>
             </div>
+            <button className="px-4 py-2 text-white bg-[#00d390] rounded-md font-semibold block mt-10">
+              Install Now ({size}MB)
+            </button>
           </div>
         </div>
         <div id="rechart"></div>
