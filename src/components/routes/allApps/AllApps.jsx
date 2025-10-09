9f2059ec23d32f314b530d77c2a3562545310b7f
Amin-ur-Rahman <aminurar679799@gmail.com>
2025-10-09 10:33:06 +0600

    search functionality added

--- src/components/routes/allApps/AllApps.jsx
+++ src/components/routes/allApps/AllApps.jsx
@@ -1,34 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import DataContext from "../../../DataContext";
 import AllAppsCard from "./AllAppsCard";
+import ErrorMessage from "../../../ErrorMessage";
 
 function AllApps() {
+  const [searchValue, setSearchValue] = useState("");
+  console.log(searchValue);
+
   const allApps = useContext(DataContext);
+
+  const filterApps = allApps.filter((app) => {
+    return app.title.toLowerCase() === ""
+      ? true
+      : app.title.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
   return (
     <div className="bg-[#f5f5f5]">
       <div className=" w-[90dvw] mx-auto flex flex-col items-center py-20 gap-3">
         <h2 className="text-[#001931] text-[clamp(1.5rem,3dvw,3rem)] font-bold text-center">
-          Trending Apps
+          All Our Applications
         </h2>
         <p className="text-center">
-          Explore All Trending Apps on the Market developed by us
+          Explore All Apps on the Market developed by us. We code for Millions
         </p>
         <div className="flex justify-between items-center  w-full">
           <h2 className="text-xl font-semibold">
             ({allApps.length}) apps found
           </h2>
           <input
+            onChange={(e) => setSearchValue(e.target.value)}
             type="text"
             name="text"
             placeholder="search apps"
             className="border border-gray-400 rounded-sm px-2 py-1"
           />
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-10 gap-5  pb-10">
-          {allApps.map((app) => (
-            <AllAppsCard app={app} key={app.id}></AllAppsCard>
-          ))}
-        </div>
+        {filterApps.length > 0 ? (
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-10 gap-5  pb-10">
+            {filterApps.map((app) => (
+              <AllAppsCard app={app} key={app.id}></AllAppsCard>
+            ))}
+          </div>
+        ) : (
+          <ErrorMessage></ErrorMessage>
+        )}
       </div>
     </div>
   );
